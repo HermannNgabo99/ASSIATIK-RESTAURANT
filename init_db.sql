c3bd6c6ae26fb21f44bcef9a2e3b83894a0cc685
1sap23 <sappelleange@gmail.com>
2026-01-08 20:58:22 +0100

    Uncomment SQL table creation statements

--- init_db.sql
+++ init_db.sql
@@ -1,11 +1,11 @@
 
-/* CREATE TABLE categorie (
+ CREATE TABLE categorie (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   image_path VARCHAR(255)
-); */
+); 
 
-/* CREATE TABLE plat (
+ CREATE TABLE plat (
   id INT AUTO_INCREMENT PRIMARY KEY,
   categorie_id INT NOT NULL,
   name VARCHAR(120) NOT NULL,
@@ -16,18 +16,18 @@
   tags VARCHAR(255),
   FOREIGN KEY (categorie_id) REFERENCES categorie(id)
 );
-*/
 
-/* CREATE TABLE commande (
+
+CREATE TABLE commande (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
   order_number VARCHAR(20) UNIQUE,
   status VARCHAR(20) NOT NULL DEFAULT 'EN_COURS',
   total DECIMAL(10, 2) DEFAULT 0.00,
   date_creation DATETIME DEFAULT CURRENT_TIMESTAMP,
   client_id VARCHAR(50)
-); */
+); 
 
-/* CREATE TABLE ligne_commande (
+ CREATE TABLE ligne_commande (
   id INT AUTO_INCREMENT PRIMARY KEY,
   commande_id BIGINT NOT NULL,
   plat_id INT NOT NULL,
@@ -36,5 +36,5 @@
   unit_price DECIMAL(10, 2) NOT NULL,
   FOREIGN KEY (commande_id) REFERENCES commande(id),
   FOREIGN KEY (plat_id) REFERENCES plat(id)
-); */
+); 
 
